e58b64cf3eeac6fdf9f4f021f6c4d21e4373f3cd
TatianaNarizhna <narizhna.tania@gmail.com>
2021-09-30 22:12:48 +0300

    w

--- model/index.js
+++ model/index.js
@@ -1,13 +1,25 @@
-// const fs = require('fs/promises')
-// const contacts = require('./contacts.json')
+const crypto = require('crypto');
+const DataBase = require('./dataBase');
+const db = new DataBase('contacts.json');
 
-const listContacts = async () => {}
+const listContacts = async () => {
+  return await db.readData()
+}
 
 const getContactById = async (contactId) => {}
 
 const removeContact = async (contactId) => {}
 
-const addContact = async (body) => {}
+const addContact = async (body) => {
+  const contacts = await db.readData()
+  const newContact = {
+    id: crypto.randomUUID(),
+    ...body,
+  }
+  contacts.push(newContact);
+  await db.writeData(contacts);
+  return newContact;
+}
 
 const updateContact = async (contactId, body) => {}
 
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,8 +1,14 @@
-const express = require('express')
-const router = express.Router()
+const express = require('express');
+const router = express.Router();
+const Contacts = require('../../model');
 
 router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  try {
+    const contacts = await Contacts.listContacts();
+    res.json({ status: 'success', code: 200, data: { contacts} })
+  } catch (error) {
+    next(error)
+  }
 })
 
 router.get('/:contactId', async (req, res, next) => {
@@ -10,7 +16,12 @@ router.get('/:contactId', async (req, res, next) => {
 })
 
 router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  try {
+    const contact = await Contacts.addContact(req.body);
+    res.status(201).json({ status: 'success', code: 201, data: { contact }})
+  } catch (error) {
+    next(error)
+  }
 })
 
 router.delete('/:contactId', async (req, res, next) => {
