c396420c1d355aa0e20ae6ba2aa14b1226e6ffcf
TatianaNarizhna <narizhna.tania@gmail.com>
2021-10-08 17:47:57 +0300

    w

--- bin/server.js
+++ bin/server.js
@@ -1,7 +1,13 @@
-const app = require('../app')
+const db = require('../config/db');
+const app = require('../app');
 
 const PORT = process.env.PORT || 3000
 
-app.listen(PORT, () => {
+db.then(() => {
+  app.listen(PORT, () => {
   console.log(`Server running. Use our API on port: ${PORT}`)
 })
+
+}).catch(error => {
+  console.log(`Server not run. Error: ${error.message}`)
+});
--- routes/api/validation.js
+++ routes/api/validation.js
@@ -1,14 +1,20 @@
 const Joi = require('joi');
+const { ValidInfoContact } = require('../../config/constant');
 
 const schemaContact = Joi.object({
-    name: Joi.string().min(3).required(),
+    name: Joi.string().min(ValidInfoContact.MIN_MANE)
+      .max(ValidInfoContact.MAX_NAME).required(),
     email: Joi.string().email().required(),
     phone: Joi.string()
       // eslint-disable-next-line prefer-regex-literals
       .pattern(new RegExp('^.[0-9]{3}. [0-9]{3}-[0-9]{4}$'))
       .required(),
+      favorite: Joi.boolean().required()
 });
 
+const schemeStatusContact = Joi.object({
+    favorite: Joi.boolean().required()
+})
 
 const validateContact = async (schema, obj, res, next) => {
     try {
@@ -21,7 +27,6 @@ const validateContact = async (schema, obj, res, next) => {
     }
 };
 
-
 const validateBody = async (schema, obj, res, next) => {
     try {
         await schema.validateAsync(obj)
@@ -33,6 +38,17 @@ const validateBody = async (schema, obj, res, next) => {
     }
 };
 
+const validateStatus = async (schema, obj, res, next) => {
+    try {
+        await schema.validateAsync(obj)
+        next()
+    } catch (error) {
+        res.status(400)
+        .json({ status: 'error', code: 400, 
+        message: 'missing field favorite' })
+    }
+};
+
 
 module.exports.validateContact = async (req, res, next) => {
    return await validateContact(schemaContact, req.body, res, next)
@@ -40,6 +56,10 @@ module.exports.validateContact = async (req, res, next) => {
 
  module.exports.validateBody = async (req, res, next) => {
     return await validateBody(schemaContact, req.body, res, next)
+ };
+
+ module.exports.validateStatus = async (req, res, next) => {
+    return await validateStatus(schemeStatusContact, req.paramas, res, next)
  };
 
 
